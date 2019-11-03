9dff9d3f1a7395b06cd3770592cd049188556a08
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-11-03 16:49:39 +0100

    Step5_ReverseASTAnswer.groovy - improving error handling

--- src/main/groovy/answers/Step5_ReverseASTAnswer.groovy
+++ src/main/groovy/answers/Step5_ReverseASTAnswer.groovy
@@ -1,7 +1,6 @@
 package answers
 
 import org.codehaus.groovy.ast.ASTNode
-import org.codehaus.groovy.ast.AnnotationNode
 import org.codehaus.groovy.ast.ClassCodeExpressionTransformer
 import org.codehaus.groovy.ast.MethodNode
 import org.codehaus.groovy.ast.expr.ConstantExpression
@@ -9,6 +8,7 @@ import org.codehaus.groovy.ast.expr.Expression
 import org.codehaus.groovy.ast.expr.MethodCallExpression
 import org.codehaus.groovy.control.CompilePhase
 import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.syntax.SyntaxException
 import org.codehaus.groovy.transform.AbstractASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
 
@@ -17,11 +17,14 @@ class Step5_ReverseASTAnswer extends AbstractASTTransformation {
 
     @Override
     void visit(final ASTNode[] nodes, final SourceUnit source) {
-        if (nodes.length != 2) return
-        if (nodes[0] instanceof AnnotationNode && nodes[1] instanceof MethodNode) {
-            def trn = new MirrorTransformer(source)
-            trn.visitMethod nodes[1]
+        def annotation = nodes[0]
+        def annotatedMethod = nodes[1]
+        if (!annotatedMethod instanceof MethodNode) {
+            source.addError new SyntaxException("Only for methods", annotation.lineNumber, annotation.columnNumber)
+            return
         }
+        def trn = new MirrorTransformer(source)
+        trn.visitMethod nodes[1]
     }
 
     private static class MirrorTransformer extends ClassCodeExpressionTransformer {
