472cd665eda654e532289662c3940fadd714c560
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-11-03 14:15:59 +0100

    Step4_AuthorASTAnswer.groovy simplifying validation

--- src/main/groovy/answers/Step4_AuthorASTAnswer.groovy
+++ src/main/groovy/answers/Step4_AuthorASTAnswer.groovy
@@ -1,7 +1,6 @@
 package answers
 
 import org.codehaus.groovy.ast.ASTNode
-import org.codehaus.groovy.ast.AnnotationNode
 import org.codehaus.groovy.ast.ClassHelper
 import org.codehaus.groovy.ast.ClassNode
 import org.codehaus.groovy.ast.expr.ConstantExpression
@@ -16,20 +15,22 @@ class Step4_AuthorASTAnswer extends AbstractASTTransformation {
 
     @Override
     void visit(final ASTNode[] nodes, final SourceUnit source) {
-        if (nodes.length != 2) return
-        if (nodes[0] instanceof AnnotationNode && nodes[1] instanceof ClassNode) {
-            def annotation = nodes[0]
-            def value = annotation.getMember 'value'
-            if (value instanceof ConstantExpression) {
-                nodes[1].addField(
-                        '$STEP4_AUTHOR_ANSWER',
-                        ACC_PUBLIC | ACC_FINAL | ACC_STATIC,
-                        ClassHelper.STRING_TYPE,
-                        value
-                )
-            } else {
-                source.addError new SyntaxException("Invalid value for annotation", annotation.lineNumber, annotation.columnNumber)
-            }
+        def annotation = nodes[0]
+        def annotatedClass = nodes[1]
+        if (!annotatedClass instanceof ClassNode) {
+            source.addError new SyntaxException("Only for classes", annotation.lineNumber, annotation.columnNumber)
+            return
         }
+        def value = annotation.getMember 'value'
+        if (!value instanceof ConstantExpression) {
+            source.addError new SyntaxException("Invalid value for annotation", annotation.lineNumber, annotation.columnNumber)
+            return
+        }
+        annotatedClass.addField(
+                '$STEP4_AUTHOR_ANSWER',
+                ACC_PUBLIC | ACC_FINAL | ACC_STATIC,
+                ClassHelper.STRING_TYPE,
+                value
+        )
     }
 }
