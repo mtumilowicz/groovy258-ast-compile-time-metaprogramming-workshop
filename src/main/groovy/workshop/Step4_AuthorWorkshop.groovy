abd399a5bffe7f46e09fc3d2f7dab4bab594c4d7
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-11-03 16:45:00 +0100

    Step4 test hints

--- src/main/groovy/workshop/Step4_AuthorWorkshop.groovy
+++ src/main/groovy/workshop/Step4_AuthorWorkshop.groovy
@@ -10,6 +10,7 @@ import java.lang.annotation.Target
 @Retention(RetentionPolicy.SOURCE)
 @Target([ElementType.TYPE])
 @GroovyASTTransformationClass('workshop.Step4_AuthorASTWorkshop')
+// implement Step4_AuthorWorkshopTest
 @interface Step4_AuthorWorkshop {
     String value()
 }
--- src/test/groovy/workshop/Step4_AuthorWorkshopTest.groovy
+++ src/test/groovy/workshop/Step4_AuthorWorkshopTest.groovy
@@ -1,17 +1,14 @@
 package workshop
 
 @Step4_AuthorWorkshop('XYZ')
-//@ASTTest(phase = CompilePhase.SEMANTIC_ANALYSIS, value = {
-//    assert node instanceof ClassNode
-//    def fn = node.getDeclaredField('$STEP4_AUTHOR_WORKSHOP')
-//    assert fn instanceof FieldNode
-//    assert fn.type == ClassHelper.STRING_TYPE
-//    assert fn.static
-//    assert fn.public
-//    assert fn.final
-//    def initialExpr = fn.initialExpression
-//    assert initialExpr instanceof ConstantExpression
-//    assert initialExpr.text == 'XYZ'
-//})
+// test annotation, hint @ASTTest, phase CompilePhase.SEMANTIC_ANALYSIS
+// assert that node is class, hint node instanceof ClassNode
+// get field $STEP4_AUTHOR_WORKSHOP, hint: node.getDeclaredField
+// assert that it is field, hint: instanceof FieldNode
+// assert that it is String, hint: type, ClassHelper.STRING_TYPE
+// assert that static, public, final
+// get value, hint: initialExpression
+// assert that the value is constant, hint: ConstantExpression
+// assert that the value is XYZ, hint: text
 class Step4_AuthorWorkshopTest {
 }
