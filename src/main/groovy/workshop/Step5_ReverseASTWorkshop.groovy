8173dd90b45ac7f3c392b7b01d9448dd1b0bffc0
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-11-03 16:56:59 +0100

    Step5_ReverseASTWorkshop.groovy hints

--- src/main/groovy/workshop/Step5_ReverseASTWorkshop.groovy
+++ src/main/groovy/workshop/Step5_ReverseASTWorkshop.groovy
@@ -1,14 +1,12 @@
 package workshop
 
 import org.codehaus.groovy.ast.ASTNode
-import org.codehaus.groovy.ast.AnnotationNode
 import org.codehaus.groovy.ast.ClassCodeExpressionTransformer
 import org.codehaus.groovy.ast.MethodNode
-import org.codehaus.groovy.ast.expr.ConstantExpression
 import org.codehaus.groovy.ast.expr.Expression
-import org.codehaus.groovy.ast.expr.MethodCallExpression
 import org.codehaus.groovy.control.CompilePhase
 import org.codehaus.groovy.control.SourceUnit
+import org.codehaus.groovy.syntax.SyntaxException
 import org.codehaus.groovy.transform.AbstractASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
 
@@ -17,11 +15,14 @@ class Step5_ReverseASTWorkshop extends AbstractASTTransformation {
 
     @Override
     void visit(final ASTNode[] nodes, final SourceUnit source) {
-        if (nodes.length != 2) return
-        if (nodes[0] instanceof AnnotationNode && nodes[1] instanceof MethodNode) {
-            def trn = new MirrorTransformer(source)
-            trn.visitMethod nodes[1]
+        def annotation = nodes[0]
+        def annotatedMethod = nodes[1]
+        if (!annotatedMethod instanceof MethodNode) {
+            source.addError new SyntaxException("Only for methods", annotation.lineNumber, annotation.columnNumber)
+            return
         }
+        def trn = new MirrorTransformer(source)
+        trn.visitMethod nodes[1]
     }
 
     private static class MirrorTransformer extends ClassCodeExpressionTransformer {
@@ -40,11 +41,10 @@ class Step5_ReverseASTWorkshop extends AbstractASTTransformation {
         @Override
         Expression transform(final Expression exp) {
             def res = super.transform exp
-            if (res instanceof MethodCallExpression && res.method instanceof ConstantExpression) {
-                res.method = new ConstantExpression(res.method.text.reverse())
-            }
-
-            res
+            // copy expression, hint: super.transform
+            // if it is method call and constant expression - reverse it
+            // hint: res.method, text, reverse
+            // otherwise simply return intact copy
         }
     }
 }
